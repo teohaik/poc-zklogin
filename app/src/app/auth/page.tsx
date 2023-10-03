65fcc0559518ec9ca558a5497b2facdeb113b500
Theodore Chaikalis <teo@mystenlabs.com>
2023-10-03 16:10:46 +0300

    add coin balance check for TX Exec button

--- app/src/app/auth/page.tsx
+++ app/src/app/auth/page.tsx
@@ -346,7 +346,7 @@ export default function Page() {
                 </dl>
 
 
-                {zkProof ? (
+                {zkProof && enoughBalance(userBalance)? (
                     <div className="pt-5">
                         <button
                             type="submit"
