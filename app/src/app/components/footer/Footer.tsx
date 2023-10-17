4e8a9cb7983428f4cf4e2b915bd98374fecf4598
Theodore Chaikalis <teo@mystenlabs.com>
2023-10-17 14:30:57 -0600

    minor footer fix

--- app/src/app/components/footer/Footer.tsx
+++ app/src/app/components/footer/Footer.tsx
@@ -8,7 +8,7 @@ const buildCommit = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA
 const Footer = () => (
     <footer className="sticky bottom-0 w-full p-4">
         <hr/>
-        {name!} - Ver: {version!} {buildCommit ? `- Build: ${buildCommit}` : null}
+        {name!} - Ver: {version!} {buildCommit ? `- Build created from : ${buildCommit}` : null}
     </footer>
 );
 
