c9f725f5abe1faffc91fa1736f16c078165f2ae6
vlegakis <vlegakis@gmail.com>
2023-11-20 17:25:55 +0200

    Redirect url

---
+++ app/src/app/proxy/page.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+export default function Page() {
+
+    const urlParamsString = window.location.search
+    const urlParams = new URLSearchParams(urlParamsString);
+    const redirect_uri = urlParams.get("redirect_uri")
+
+    if (redirect_uri) {
+        // console.log("Got into redirect:", redirect_uri)
+        window.location.replace(redirect_uri)
+        return <></>
+    }
+
+    return (
+        <>
+            <span>Redirecting failed. Check URL redirect_uri param</span>
+        </>
+    );
+}
